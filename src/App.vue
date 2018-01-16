ef0ed42094f88cda683b37cebaab5a784df0344c
rchaser53 <tayoshizawa29@gmail.com>
2018-01-16 22:44:22 +0900

    try to use case

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
 <template lang="pug">
   div(class='hello') nyan
+    // html is cached so need to change when reflesh
+    //- this comment is erased after conversion
+    - var random = Math.round(Math.random()*3)
+    case random
+      when 0
+        div 0
+      when 1
+        div 1
+      default
+        div above 2
+    h1 {{hoge}}
 </template>
 
 <script>
 export default {
   name: 'App',
+  data() {
+    return {
+      hoge: 22
+    }
+  },
   components: {}
 }
 </script>
